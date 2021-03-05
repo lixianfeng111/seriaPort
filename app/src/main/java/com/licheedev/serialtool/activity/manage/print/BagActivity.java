1d80a304c1323b0698b7c84c5f46b84d8a56418b
lixianfeng111 <17801130965@163.com>
2021-03-05 11:32:01 +0800

    当前钞袋详情完成

--- app/src/main/java/com/licheedev/serialtool/activity/manage/print/BagActivity.java
+++ app/src/main/java/com/licheedev/serialtool/activity/manage/print/BagActivity.java
@@ -2,9 +2,9 @@ package com.licheedev.serialtool.activity.manage.print;
 
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-import com.licheedev.serialtool.AppManager;
 import com.licheedev.serialtool.R;
 import com.licheedev.serialtool.base.BaseActivity;
 import com.licheedev.serialtool.base.BasePresenter;
@@ -15,6 +15,11 @@ import butterknife.BindView;
 
 public class BagActivity extends BaseActivity {
 
+
+    @BindView(R.id.cnr)
+    RelativeLayout relativeLayout_cnr;
+    @BindView(R.id.mxn)
+    RelativeLayout relativeLayout_mxn;
     //张数
     @BindView(R.id.piece_100)
     TextView piece_100;
@@ -29,6 +34,18 @@ public class BagActivity extends BaseActivity {
     @BindView(R.id.piece_1)
     TextView piece_1;
 
+    @BindView(R.id.piece_1000_mxn)
+    TextView piece_1000_mxn;
+    @BindView(R.id.piece_500_mxn)
+    TextView piece_500_mxn;
+    @BindView(R.id.piece_200_mxn)
+    TextView piece_200_mxn;
+    @BindView(R.id.piece_100_mxn)
+    TextView piece_100_mxn;
+    @BindView(R.id.piece_50_mxn)
+    TextView piece_50_mxn;
+    @BindView(R.id.piece_20_mxn)
+    TextView piece_20_mxn;
     //金额
     @BindView(R.id.piece100_sum)
     TextView piece100_sum;
@@ -43,6 +60,34 @@ public class BagActivity extends BaseActivity {
     @BindView(R.id.piece1_sum)
     TextView piece1_sum;
 
+    @BindView(R.id.piece1000_sum_mxn)
+    TextView piece1000_sum_mxn;
+    @BindView(R.id.piece500_sum_mxn)
+    TextView piece500_sum_mxn;
+    @BindView(R.id.piece200_sum_mxn)
+    TextView piece200_sum_mxn;
+    @BindView(R.id.piece100_sum_mxn)
+    TextView piece100_sum_mxn;
+    @BindView(R.id.piece50_sum_mxn)
+    TextView piece50_sum_mxn;
+    @BindView(R.id.piece20_sum_mxn)
+    TextView piece20_sum_mxn;
+
+    //总数
+    @BindView(R.id.amount)
+    TextView amount;
+    @BindView(R.id.piece_amount)
+    TextView piece_amount;
+    @BindView(R.id.amount_sum)
+    TextView amount_sum;
+
+    @BindView(R.id.amount_mxn)
+    TextView amount_mxn;
+    @BindView(R.id.piece_amount_mxn)
+    TextView piece_amount_mxn;
+    @BindView(R.id.amount_sum_mxn)
+    TextView amount_sum_mxn;
+
     //其他存款
     @BindView(R.id.coin_sum)
     TextView coin_sum;
@@ -53,6 +98,20 @@ public class BagActivity extends BaseActivity {
     @BindView(R.id.other_sum)
     TextView other_sum;
 
+    @BindView(R.id.coin_sum_mxn)
+    TextView coin_sum_mxn;
+    @BindView(R.id.check_sum_mxn)
+    TextView check_sum_mxn;
+    @BindView(R.id.banknote_sum_mxn)
+    TextView banknote_sum_mxn;
+    @BindView(R.id.other_sum_mxn)
+    TextView other_sum_mxn;
+
+    //总计
+    @BindView(R.id.total_CNR)
+    TextView total_CNR;
+    @BindView(R.id.total_MXN)
+    TextView total_MXN;
     //返回按钮
     @BindView(R.id.btnBack)
     Button btnBack;
@@ -131,23 +190,66 @@ public class BagActivity extends BaseActivity {
         //得到墨西哥币总计
         totalAll_mxn=total_mxn+coin2_mxn+cheque2_mxn+paper_money2_mxn+other2_mxn;
 
-        piece_100.setText(num100+"");
-        piece_50.setText(num50+"");
-        piece_20.setText(num20+"");
-        piece_10.setText(num10+"");
-        piece_5.setText(num5+"");
-        piece_1.setText(num1+"");
-        piece100_sum.setText(money100+"");
-        piece50_sum.setText(money50+"");
-        piece20_sum.setText(money20+"");
-        piece10_sum.setText(money10+"");
-        piece5_sum.setText(money5+"");
-        piece1_sum.setText(money1+"");
+        //人民币
+        if (totalAll>0){
+            relativeLayout_cnr.setVisibility(View.VISIBLE);
+            //张数
+            piece_100.setText(num100+"");
+            piece_50.setText(num50+"");
+            piece_20.setText(num20+"");
+            piece_10.setText(num10+"");
+            piece_5.setText(num5+"");
+            piece_1.setText(num1+"");
+            //金额
+            piece100_sum.setText(money100+"");
+            piece50_sum.setText(money50+"");
+            piece20_sum.setText(money20+"");
+            piece10_sum.setText(money10+"");
+            piece5_sum.setText(money5+"");
+            piece1_sum.setText(money1+"");
+            //总数
+            piece_amount.setText(num+"");
+            amount_sum.setText(total+"");
+            //其他存款
+            coin_sum.setText(coin2+"");
+            check_sum.setText(cheque2+"");
+            banknote_sum.setText(paper_money2+"");
+            other_sum.setText(other2+"");
+            //总计
+            total_CNR.setText(totalAll+"");
+        }else {
+            relativeLayout_cnr.setVisibility(View.GONE);
+        }
+        //墨西哥币
+        if (totalAll_mxn>0){
+            relativeLayout_mxn.setVisibility(View.VISIBLE);
+            //张数
+            piece_1000_mxn.setText(num_mxn1000+"");
+            piece_500_mxn.setText(num_mxn500+"");
+            piece_200_mxn.setText(num_mxn200+"");
+            piece_100_mxn.setText(num_mxn100+"");
+            piece_50.setText(num50+"");
+            piece_20_mxn.setText(num_mxn20+"");
+            //金额
+            piece1000_sum_mxn.setText(money_mxn1000+"");
+            piece500_sum_mxn.setText(money_mxn500+"");
+            piece200_sum_mxn.setText(money_mxn200+"");
+            piece100_sum_mxn.setText(money_mxn100+"");
+            piece50_sum_mxn.setText(money_mxn50+"");
+            piece20_sum_mxn.setText(money_mxn20+"");
+            piece_amount_mxn.setText(num_mxn+"");
+            amount_sum_mxn.setText(total_mxn+"");
+            //其他存款
+            coin_sum_mxn.setText(coin2_mxn+"");
+            check_sum_mxn.setText(cheque2_mxn+"");
+            banknote_sum_mxn.setText(paper_money2_mxn+"");
+            other_sum_mxn.setText(other2_mxn+"");
 
-        coin_sum.setText(coin2+"");
-        check_sum.setText(cheque2+"");
-        banknote_sum.setText(paper_money2+"");
-        other_sum.setText(other2+"");
+            total_MXN.setText(totalAll_mxn+"");
+        }else {
+            relativeLayout_mxn.setVisibility(View.GONE);
+        }
+
     }
 
     @Override
